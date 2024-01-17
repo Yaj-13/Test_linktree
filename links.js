f95fc7bf3c7b075838408214def0f302ae6ec2e4
yaj-13 <tu4200988@taibahu.edu.sa>
2024-01-17 19:58:33 +0300

    first commit

---
+++ links.js
@@ -0,0 +1,26 @@
+export const links = [ 
+	{ 
+		name: "X", 
+		link: 
+"https://twitter.com/k4y_5", 
+		image: 
+"pics/twitter.png", 
+	}, 
+	{ 
+		name: "Linkedin", 
+		link: 
+"https://www.linkedin.com/in/yazeed-aloufi-1a1275263/", 
+		image: 
+"pics/linkedin.png", 
+	}, 
+	{ 
+		name: "Instagram", 
+		link: 
+"https://www.instagram.com/k4y_5/?hl=en", 
+		image: 
+"pics/instagram.png", 
+	}, 
+ 
+
+	
+];
